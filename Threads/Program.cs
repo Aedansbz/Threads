e619ddcabda4a8a9b87d1eda64107212473e3861
Saleh <Saleh@DESKTOP-UIL5HQK>
2024-10-22 19:44:44 +0330

    Add project files.

---
+++ Threads/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    internal class Program
+    {
+        static void Counter(string threadname)
+        {
+            for (int i = 0; i <= 100 ; i++)
+            {
+                Console.WriteLine($"{threadname}: {i}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(() => Counter("Thread 1"));
+            thread1.Start();
+            Thread thread2 = new Thread(() => Counter("Thread 2"));
+            thread2.Start();
+            Thread thread3 = new Thread(() => Counter("Thread 3"));
+            thread3.Start();
+            Console.ReadKey();
+        }
+    }
+}
